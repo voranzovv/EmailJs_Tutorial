8e0fdfa9a8c1d06ac82da96d4507f48f6b03ccbc
voranzovv <voranzov@gmail.com>
2022-09-14 05:46:19 +0530

    EmailJs tutorial

---
+++ index.js
@@ -0,0 +1,23 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "YOUR_SERVICE_ID";
+  const templateID = "YOUR_TEMPLETE_ID";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
+
